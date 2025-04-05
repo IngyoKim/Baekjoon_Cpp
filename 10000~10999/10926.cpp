c0e70696dd6e4e5e0756258fb3ed9127e9ccd07b
IngyoKim <a58276976@gmail.com>
2025-04-05 18:15:48 +0900

    ??!

--- 10000~10999/10926.cpp
+++ 10000~10999/10926.cpp
@@ -1,11 +1,11 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	string name;
-//	
-//	cin >> name;
-//
-//	cout << name << "??!";
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name;
+	
+	cin >> name;
+
+	cout << name << "??!";
+}
