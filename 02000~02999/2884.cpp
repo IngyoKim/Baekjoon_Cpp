dc44ebcb44db503e0ed16ee259ea660361117bdd
IngyoKim <a58276976@gmail.com>
2025-04-05 18:14:12 +0900

    알람 시계

--- 02000~02999/2884.cpp
+++ 02000~02999/2884.cpp
@@ -1,28 +1,28 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int hour, minute;
-//
-//	cin >> hour >> minute; 
-//
-//	if (minute - 45 < 0)
-//	{
-//		if (hour == 0)
-//		{
-//			hour = 23;
-//		}
-//		else
-//		{
-//			hour -= 1;
-//		}
-//		minute += 15;
-//	}
-//	else
-//	{
-//		minute -= 45;
-//	}
-//
-//	cout << hour << " " << minute;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int hour, minute;
+
+	cin >> hour >> minute; 
+
+	if (minute - 45 < 0)
+	{
+		if (hour == 0)
+		{
+			hour = 23;
+		}
+		else
+		{
+			hour -= 1;
+		}
+		minute += 15;
+	}
+	else
+	{
+		minute -= 45;
+	}
+
+	cout << hour << " " << minute;
+}
