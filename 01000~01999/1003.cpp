614fe72fa883c7e8eb7a8672f21c29645570fdbd
IngyoKim <a58276976@gmail.com>
2025-04-05 18:08:58 +0900

    피보나치 함수

--- 01000~01999/1003.cpp
+++ 01000~01999/1003.cpp
@@ -1,30 +1,30 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int testcase;
-//	int fibo[50] = { 0 , 1, 1 };
-//	for (int i = 3; i < 50; i++)
-//	{
-//		fibo[i] = fibo[i - 2] + fibo[i - 1];
-//	}
-//
-//	cin >> testcase;
-//
-//	for (int i = 0; i < testcase; i++)
-//	{
-//		int inputNum;
-//
-//		cin >> inputNum;
-//
-//		if (inputNum == 0)
-//		{
-//			cout << 1 << " " << fibo[inputNum] << endl;
-//		}
-//		else
-//		{
-//			cout << fibo[inputNum - 1] << " " << fibo[inputNum] << endl;
-//		}
-//	}
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int testcase;
+	int fibo[50] = { 0 , 1, 1 };
+	for (int i = 3; i < 50; i++)
+	{
+		fibo[i] = fibo[i - 2] + fibo[i - 1];
+	}
+
+	cin >> testcase;
+
+	for (int i = 0; i < testcase; i++)
+	{
+		int inputNum;
+
+		cin >> inputNum;
+
+		if (inputNum == 0)
+		{
+			cout << 1 << " " << fibo[inputNum] << endl;
+		}
+		else
+		{
+			cout << fibo[inputNum - 1] << " " << fibo[inputNum] << endl;
+		}
+	}
+}
