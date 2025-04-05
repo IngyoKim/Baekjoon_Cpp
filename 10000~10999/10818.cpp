0a00a7060087effb6985964d36f276678d5e20d8
IngyoKim <a58276976@gmail.com>
2025-04-05 17:09:58 +0900

    나머지

---
+++ 10000~10999/10818.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, arr[1000000],min = 1000000, max = -1000000;
+//
+//	cin >> a;
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//		
+//		if (min > arr[i])
+//		{
+//			min = arr[i];
+//		}
+//	}
+//	cout << min << " " << max;
+//}
