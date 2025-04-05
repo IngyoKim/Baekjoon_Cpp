84f6d738582597ea217aed51e3de3f3bbb320b39
IngyoKim <a58276976@gmail.com>
2025-04-05 18:14:32 +0900

    나머지

--- 03000~03999/3052.cpp
+++ 03000~03999/3052.cpp
@@ -1,38 +1,38 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int arr[10], a, cnt = 0;
-//
-//	for (int i = 0; i < 10; i++)
-//	{
-//		cin >> a;
-//		arr[i] = a % 42;
-//	}
-//
-//	for (int i = 0; i < 10; i++)
-//	{
-//		int flag = 1;
-//		for (int j = 0; j < 10; j++)
-//		{
-//			if (arr[i] == arr[j])
-//			{
-//				if (i != j)
-//				{
-//					arr[j] = -1;
-//				}
-//			}
-//		}
-//	}
-//
-//	for (int i = 0; i < 10; i++)
-//	{
-//		if (arr[i] == -1)
-//		{
-//			cnt++;
-//		}
-//	}
-//
-//	cout << 10-cnt;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[10], a, cnt = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> a;
+		arr[i] = a % 42;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		int flag = 1;
+		for (int j = 0; j < 10; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				if (i != j)
+				{
+					arr[j] = -1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] == -1)
+		{
+			cnt++;
+		}
+	}
+
+	cout << 10-cnt;
+}
