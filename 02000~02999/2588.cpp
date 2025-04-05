2bb1a9b4cc82820b7bd198e3c04acd5b23fb96cd
IngyoKim <a58276976@gmail.com>
2025-04-05 18:13:52 +0900

    곱셈

--- 02000~02999/2588.cpp
+++ 02000~02999/2588.cpp
@@ -1,17 +1,17 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int a, b;
-//
-//	cin >> a >> b;
-//
-//	int bFirst, bSecond, bThird;
-//
-//	bThird = b % 10;
-//	bSecond = ((b % 100) - bThird)/10;
-//	bFirst = ((b % 1000) - (10 * bSecond) - bThird) / 100;
-//
-//	cout << a * bThird << "\n" << a * bSecond << "\n" << a * bFirst << "\n" << a*b;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	int bFirst, bSecond, bThird;
+
+	bThird = b % 10;
+	bSecond = ((b % 100) - bThird)/10;
+	bFirst = ((b % 1000) - (10 * bSecond) - bThird) / 100;
+
+	cout << a * bThird << "\n" << a * bSecond << "\n" << a * bFirst << "\n" << a*b;
+}
