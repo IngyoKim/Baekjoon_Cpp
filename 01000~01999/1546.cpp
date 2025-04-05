8496cfb79c0ded0924085af3b950d8f7e07c378e
IngyoKim <a58276976@gmail.com>
2025-04-05 18:12:46 +0900

    평균

--- 01000~01999/1546.cpp
+++ 01000~01999/1546.cpp
@@ -1,26 +1,26 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int sub;
-//	float arr[1000], max = 0, sum = 0;
-//
-//	cin >> sub;
-//
-//	for (int i = 0; i < sub; i++)
-//	{
-//		cin >> arr[i];
-//		if (max < arr[i])
-//		{
-//			max = arr[i];
-//		}
-//	}
-//
-//	for (int i = 0; i < sub; i++)
-//	{
-//		sum += arr[i] / max * 100;
-//	}
-//
-//	cout << sum / sub;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sub;
+	float arr[1000], max = 0, sum = 0;
+
+	cin >> sub;
+
+	for (int i = 0; i < sub; i++)
+	{
+		cin >> arr[i];
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+
+	for (int i = 0; i < sub; i++)
+	{
+		sum += arr[i] / max * 100;
+	}
+
+	cout << sum / sub;
+}
